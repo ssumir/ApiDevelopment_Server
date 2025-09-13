de4fee19cb087e704df1c26bebfec2b2b83c881e
ssumir <145274759+ssumir@users.noreply.github.com>
2025-09-13 16:42:49 +0600

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -28,15 +28,13 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'   // or HashRouter
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/ApiDevelopment_Server">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
 )
-
-
