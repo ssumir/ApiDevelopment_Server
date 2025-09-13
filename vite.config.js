22811c4d7534aed37e4b8393a04c5a4b9e60642b
ssumir <145274759+ssumir@users.noreply.github.com>
2025-09-13 17:29:56 +0600

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ApiDevelopment_Server/",
-})
+  // You should remove the 'base' property for HashRouter
+});
